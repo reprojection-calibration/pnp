201d220ce29a0cec88fd3a07076bedde5d884bd9
reprojection-calibration <reprojection.calibration@gmail.com>
2025-09-04 16:15:51 +0200

    extract A into its own method

--- code/src/dlt.test.cpp
+++ code/src/dlt.test.cpp
@@ -38,15 +38,17 @@ Eigen::MatrixXd InterleaveRowWise(Eigen::MatrixXd const& matrix) {
     return interleaved_matrix;
 }
 
-// NOTE(Jack): The number of pixels and points has to match! However, because Dlt is part of the internal API, and the
-// number of correspondences is already check in the public facing interface, we do not check it again here.
-Eigen::Isometry3d Dlt(Eigen::MatrixX2d const& pixels, Eigen::MatrixX3d const& points) {
-    // The 2n x 12 matrix assembled by stacking up the constraints from (MVG Eq. 7.2)
+// The 2n x 12 matrix assembled by stacking up the constraints from (MVG Eq. 7.2)
+// NOTE(Jack): I am not gonna test this because I hope this function changes soon, see the note in the function.
+Eigen::Matrix<double, Eigen::Dynamic, 12> ConstructA(Eigen::MatrixX2d const& pixels, Eigen::MatrixX3d const& points) {
     Eigen::Matrix<double, Eigen::Dynamic, 12> A(2 * pixels.rows(), 12);
     for (int const i : {0, 1, 2}) {
         A.middleCols(i * 4, 4) = InterleaveRowWise(points).rowwise().homogeneous();
     }
 
+    // TODO(Jack): There has to be a better way to do this than iterating over each pixel and doing this manually! This
+    // entire function upsets me because it feels so manual, and that we are missing the proper linear algebra
+    // abstraction to combine the matrices into A
     for (Eigen::Index i{0}; i < pixels.rows(); ++i) {
         auto const pixel_i{pixels.row(i)};
         auto A_i{A.middleRows(i * 2, 2)};
@@ -56,12 +58,21 @@ Eigen::Isometry3d Dlt(Eigen::MatrixX2d const& pixels, Eigen::MatrixX3d const& po
         A_i.block<1, 4>(0, 4) *= -1.0;
         A_i.block<1, 4>(0, 8) *= pixel_i(1);
 
-        // Construct w_i*X_i^T, O^T, x_i * X_i^T
+        // Construct w_i*X_i^T, O^T, -x_i * X_i^T
         A_i.block<1, 4>(1, 0) *= 1.0;
         A_i.block<1, 4>(1, 4) = Eigen::ArrayXXd::Zero(1, 4);
         A_i.block<1, 4>(1, 8) *= -pixel_i(0);
     }
 
+    return A;
+}
+
+// NOTE(Jack): The number of pixels and points has to match! However, because Dlt is part of the internal API, and the
+// number of correspondences is already check in the public facing interface, we do not check it again here.
+Eigen::Isometry3d Dlt(Eigen::MatrixX2d const& pixels, Eigen::MatrixX3d const& points) {
+    Eigen::Matrix<double, Eigen::Dynamic, 12> const A{ConstructA(pixels, points)};
+
+    std::cout << A << std::endl;
     return Eigen::Isometry3d::Identity();
 }
 
