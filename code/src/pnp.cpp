521a26813e56ca016757d6eb2bf5db4734d0b545
reprojection-calibration <reprojection.calibration@gmail.com>
2025-09-04 13:08:58 +0200

    add public interface and error handling

--- code/src/pnp.cpp
+++ code/src/pnp.cpp
@@ -2,6 +2,14 @@
 
 namespace reprojection_calibration::pnp {
 
-void Delete() { return; }
+PnpResult Pnp(Eigen::MatrixX2d const& pixels, Eigen::MatrixX3d const& points) {
+    if (not(pixels.rows() == points.rows())) {
+        return PnpStatusCode::MismatchedCorrespondence;
+    } else if (pixels.rows() < 6) {
+        return PnpStatusCode::NotEnoughPoints;
+    }
+
+    return Eigen::Isometry3d::Identity();
+}
 
 }  // namespace reprojection_calibration::pnp
--- code/test/test_pnp.cpp
+++ code/test/test_pnp.cpp
@@ -4,7 +4,22 @@
 
 using namespace reprojection_calibration::pnp;
 
-TEST(ZZZ, XXX) {
-    Delete();
-    EXPECT_TRUE(true);
+TEST(TestPnpErrorHandling, MismatchedCorrespondence) {
+    Eigen::MatrixX2d const four_pixels(4, 2);
+    Eigen::MatrixX3d const five_points(5, 3);
+    PnpResult const pnp_result{Pnp(four_pixels, five_points)};
+
+    EXPECT_TRUE(std::holds_alternative<PnpStatusCode>(pnp_result));
+    PnpStatusCode const pnp_status_code{std::get<PnpStatusCode>(pnp_result)};
+    EXPECT_EQ(PnpStatusCode::MismatchedCorrespondence, pnp_status_code);
+}
+
+TEST(TestPnpErrorHandling, NotEnoughPoints) {
+    Eigen::MatrixX2d const five_pixels(5, 2);
+    Eigen::MatrixX3d const five_points(5, 3);
+    PnpResult const pnp_result{Pnp(five_pixels, five_points)};
+
+    EXPECT_TRUE(std::holds_alternative<PnpStatusCode>(pnp_result));
+    PnpStatusCode const pnp_status_code{std::get<PnpStatusCode>(pnp_result)};
+    EXPECT_EQ(PnpStatusCode::NotEnoughPoints, pnp_status_code);
 }
