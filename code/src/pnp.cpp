1d137a50214b05986eb65783fc4fec6167e63304
reprojection-calibration <reprojection.calibration@gmail.com>
2025-09-05 22:22:24 +0200

    add thinking notes

--- code/src/pnp.cpp
+++ code/src/pnp.cpp
@@ -2,6 +2,12 @@
 
 namespace reprojection_calibration::pnp {
 
+// TODO(Jack): What is the canonical way to deal with the camera matrix? I want the pnp algo to know nothing about K if
+// possible, because the more we let information about the camera and camera models percolate through the code base, the
+// more combined everything will get. That being said, should we specifically pass the pixels here in image coordinates?
+// That seems to be the only reasonable coordinate system given how people interact with pixels. Then the next question
+// I have is should we normalize the points at the top level pnp function and then feed those both to the DLT and the
+// nonlinear refinement? Right now I do not understand what coordinate context to do the nonlinear refinement in.
 PnpResult Pnp(Eigen::MatrixX2d const& pixels, Eigen::MatrixX3d const& points) {
     if (not(pixels.rows() == points.rows())) {
         return PnpStatusCode::MismatchedCorrespondence;
