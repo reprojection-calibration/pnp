a63a82755a2c118c0247e7c9b91bbf121305460e
reprojection-calibration <reprojection.calibration@gmail.com>
2025-09-05 13:56:17 +0200

    add comment for column wise normalization

--- code/src/matrix_utilities.hpp
+++ code/src/matrix_utilities.hpp
@@ -21,6 +21,9 @@ namespace reprojection_calibration::pnp {
 //          4, 5
 Eigen::MatrixXd InterleaveRowWise(Eigen::MatrixXd const& matrix);
 
+// This function was designed with the normalization required for the DLT in mind! This means that after normalization
+// we want the "average distance of a point x from the origin is equal to sqrt(n)" (where n is the dimension of the
+// non-homogeneous point). See MVG 4.4.4 heading "Isotropic Scaling"
 Eigen::MatrixXd NormalizeColumnWise(Eigen::MatrixXd const& matrix);
 
 }  // namespace reprojection_calibration::pnp
