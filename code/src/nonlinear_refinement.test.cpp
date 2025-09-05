0e8d352a154eb3197b3318747276c1914fe0d1be
reprojection-calibration <reprojection.calibration@gmail.com>
2025-09-05 21:49:34 +0200

    add nonlinear refinement boilerplate

---
+++ code/src/nonlinear_refinement.test.cpp
@@ -0,0 +1,21 @@
+#include <gtest/gtest.h>
+
+#include "dlt.hpp"
+#include "test_data.hpp"
+
+namespace reprojection_calibration::pnp {
+
+Eigen::Isometry3d NonlinearRefinement(Eigen::MatrixX2d const& pixels, Eigen::MatrixX3d const& points,
+                                      Eigen::Isometry3d const& tf_initial) {
+    (void)pixels;
+    (void)points;
+    (void)tf_initial;
+
+    return Eigen::Isometry3d::Identity();
+}
+
+}  // namespace reprojection_calibration::pnp
+
+using namespace reprojection_calibration::pnp;
+
+TEST(ZZZ, XXX) { EXPECT_EQ(1, 2); }
