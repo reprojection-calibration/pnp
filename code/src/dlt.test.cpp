7caa9c4b1ba29fb96013fcc1042b046d062b5b50
reprojection-calibration <reprojection.calibration@gmail.com>
2025-09-05 16:46:48 +0200

    clang format

--- code/src/dlt.test.cpp
+++ code/src/dlt.test.cpp
@@ -52,6 +52,6 @@ TEST(TestDlt, XXX) {
 
     EXPECT_FLOAT_EQ(tf.linear().determinant(), 1);
     EXPECT_NEAR(tf.linear().diagonal().norm(), std::sqrt(3), 1e-3);  // Heuristic given the provided test data
-    EXPECT_TRUE(tf.translation().isApprox(
-        Eigen::Vector3d{5.22379e-05, -2.73534e-05, 0.215797}, 1e-3));  // Heuristic given the provided test data
+    EXPECT_TRUE(tf.translation().isApprox(Eigen::Vector3d{5.22379e-05, -2.73534e-05, 0.215797},
+                                          1e-3));  // Heuristic given the provided test data
 }
