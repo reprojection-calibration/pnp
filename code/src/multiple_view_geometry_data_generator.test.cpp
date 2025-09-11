2c6f638063390d7753b728090e3adf03cf795a4e
reprojection-calibration <reprojection.calibration@gmail.com>
2025-09-11 22:30:34 +0200

    add FromSe3 tests

--- code/src/multiple_view_geometry_data_generator.test.cpp
+++ code/src/multiple_view_geometry_data_generator.test.cpp
@@ -23,13 +23,21 @@ Se3 ToSe3(Eigen::Isometry3d const& matrix) {
     return pose;
 }
 
-// Eigen::Isometry3d FromSe3(Se3 const& se3) { return Eigen::Isometry3d::Identity(); }
+Eigen::Isometry3d FromSe3(Se3 const& se3) {
+    Eigen::Isometry3d pose;
+
+    Eigen::Vector3d const so3{se3.topRows(3)};
+    pose.linear() = Eigen::AngleAxisd(so3.norm(), so3.normalized()).toRotationMatrix();
+    pose.translation() = se3.bottomRows(3);
+
+    return pose;
+}
 
 }  // namespace reprojection_calibration::pnp
 
 using namespace reprojection_calibration::pnp;
 
-TEST(HHH, XXX) {
+TEST(HHH, TestToSe3) {
     Eigen::Isometry3d const identity{Eigen::Isometry3d::Identity()};
     EXPECT_FLOAT_EQ(ToSe3(identity).sum(), 0.0);
 
@@ -51,4 +59,19 @@ TEST(HHH, XXX) {
     translation_and_rotation.translation() = Eigen::Vector3d(-1, 0, 1);
     translation_and_rotation.rotate(Eigen::AngleAxisd(0.5 * EIGEN_PI, Eigen::Vector3d::UnitZ()));
     EXPECT_TRUE(ToSe3(translation_and_rotation).isApprox(Se3{0, 0, 0.5 * EIGEN_PI, -1, 0, 1}));
+}
+
+TEST(HHH, TestFromSe3) {
+    Se3 const identity{0, 0, 0, 0, 0, 0};
+    EXPECT_FLOAT_EQ(FromSe3(identity).matrix().diagonal().sum(), 4.0);
+
+    // Test the reversability - this is easier than trying to engineer some SE3 input matrices
+    Se3 const rotations{1, 1, 1, 0, 0, 0};
+    EXPECT_TRUE(ToSe3(FromSe3(rotations)).isApprox(rotations));
+
+    Se3 const translations{0, 0, 0, 1, 1, 1};
+    EXPECT_TRUE(ToSe3(FromSe3(translations)).isApprox(translations));
+
+    Se3 const rotations_and_translations{1, 1, 1, 1, 1, 1};
+    EXPECT_TRUE(ToSe3(FromSe3(rotations_and_translations)).isApprox(rotations_and_translations));
 }
