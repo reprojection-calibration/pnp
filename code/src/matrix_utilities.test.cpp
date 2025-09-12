eabf93d65bc0a873dac6f0a618101f3abb7af056
reprojection-calibration <reprojection.calibration@gmail.com>
2025-09-12 20:12:05 +0200

    add DecomposeIntoRk

--- code/src/matrix_utilities.test.cpp
+++ code/src/matrix_utilities.test.cpp
@@ -29,7 +29,7 @@ TEST(MatrixUtilities, TestNormalizeColumnWise) {
 }
 
 // Inspired by https://www.physicsforums.com/threads/rq-decomposition-from-qr-decomposition.261739/
-// Good link https://ksimek.github.io/2012/08/14/decompose/
+// We implement RQ decomposition in terms of Eigen's built in QR decomposition
 std::tuple<Eigen::Matrix3d, Eigen::Matrix3d> RqDecomposition(Eigen::Matrix3d const& matrix) {
     Eigen::Matrix3d const reverse_rows{{0, 0, 1}, {0, 1, 0}, {1, 0, 0}};
     Eigen::Matrix3d const reversed{reverse_rows * matrix};
@@ -41,20 +41,42 @@ std::tuple<Eigen::Matrix3d, Eigen::Matrix3d> RqDecomposition(Eigen::Matrix3d con
     Eigen::Matrix3d const R_star{reverse_rows * R.transpose() * reverse_rows};
     Eigen::Matrix3d const Q_star{reverse_rows * Q.transpose()};
 
-    return std::make_tuple(R_star, Q_star);
+    return {R_star, Q_star};
+}
+
+// NOTE(Jack): MVG section "6.2.4 Decomposition of the camera matrix" refers to the first three columns of the camera
+// matrix P as M.
+// Adopted from https://ksimek.github.io/2012/08/14/decompose/
+std::tuple<Eigen::Matrix3d, Eigen::Matrix3d> DecomposeIntoKr(Eigen::Matrix3d const& M) {
+    const auto [K, R]{RqDecomposition(M)};
+
+    // TODO(Jack): Fix hacky sign names here
+    Eigen::Vector3d const signage{K.diagonal().array().sign()};
+    Eigen::Matrix3d sign_mat{Eigen::Matrix3d::Identity()};
+    sign_mat.diagonal() = signage;
+
+    Eigen::Matrix3d const K_star{K * sign_mat};
+    Eigen::Matrix3d const R_star{sign_mat * R};
+
+    return {K_star, R_star};
 }
 
 TEST(MatrixUtilities, TestRqDecomposition) {
     Eigen::Matrix3d const M{{600, 0, 360}, {0, 600, 240}, {0, 0, 1}};
 
-    auto [K, R]{RqDecomposition(M)};
+    auto const [R, Q]{RqDecomposition(M)};
 
-    Eigen::Vector3d const signage = K.diagonal().array().sign();
-    Eigen::Matrix3d sign_mat = Eigen::Matrix3d::Identity();
-    sign_mat.diagonal() = signage;
+    EXPECT_TRUE(R.isUpperTriangular());
+    EXPECT_FLOAT_EQ((Q * Q.transpose()).diagonal().sum(), 3.0);  // Matrix is orthogonal - Q*Q^T returns identity
+}
 
-    K = K * sign_mat;
-    R = sign_mat * R;
+TEST(MatrixUtilities, TestDecomposeIntoRk) {
+    Eigen::Matrix3d const M{{600, 0, 360}, {0, 600, 240}, {0, 0, 1}};
 
-    EXPECT_EQ(1, 2);
+    auto const [K, R]{DecomposeIntoKr(M)};
+
+    EXPECT_TRUE(K.isUpperTriangular());
+    EXPECT_EQ(K.diagonal().array().sign().sum(), 3);  // All diagonal entries of K must be positive
+    EXPECT_FLOAT_EQ((R * R.transpose()).diagonal().sum(), 3.0);
+    EXPECT_FLOAT_EQ(R.determinant(), 1.0);
 }
