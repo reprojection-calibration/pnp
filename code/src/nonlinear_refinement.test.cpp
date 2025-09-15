081bb81fb0c40a27414a25d718ff303da9612907
reprojection-calibration <reprojection.calibration@gmail.com>
2025-09-15 16:19:50 +0200

    add TransformPoints

--- code/src/nonlinear_refinement.test.cpp
+++ code/src/nonlinear_refinement.test.cpp
@@ -6,6 +6,8 @@
 
 namespace reprojection_calibration::pnp {
 
+// TODO(Jack): What is the final and best type for camera going to be? Raw pointer smells to me, or is at least not 100%
+// necessary considering how far along with ceres we are (not far).
 template <typename T>
 Eigen::Vector<T, 2> PinholeProjection(T const* const camera, Eigen::Vector<T, 3> const& point) {
     T const& fx{camera[0]};
@@ -17,15 +19,51 @@ Eigen::Vector<T, 2> PinholeProjection(T const* const camera, Eigen::Vector<T, 3>
     T const& y{point[1]};
     T const& z{point[2]};
 
+    // TODO(Jack): Can/should we replace this with eigen matrix operations?
     T const u{(fx * x / z) + cx};
     T const v{(fy * y / z) + cy};
 
     return {u, v};
 }
 
+// TODO(Jack): Can we use the se3 type here?
+template <typename T>
+Eigen::Vector<T, 3> TransformPoint(Eigen::Vector<T, 6> const& tf, Eigen::Vector<T, 3> const& point) {
+    Eigen::Vector<T, 3> const axis_angle{tf.topRows(3)};
+    Eigen::Vector<T, 3> transformed_point;
+    ceres::AngleAxisRotatePoint(axis_angle.data(), point.data(), transformed_point.data());
+
+    Eigen::Vector<T, 3> const translation{tf.bottomRows(3)};
+    transformed_point += translation;
+
+    return transformed_point;
+}
+
 }  // namespace reprojection_calibration::pnp
 
 using namespace reprojection_calibration::pnp;
+
+TEST(NonlinearRefinement, TestTransformPointsTranslation) {
+    Eigen::Vector<double, 6> const tf{0, 0, 0, 1, 2, 3};  // Translation only
+    Eigen::Vector3d const point{5, 10, 15};
+
+    Eigen::Vector<double, 3> const transformed_point{TransformPoint(tf, point)};
+
+    EXPECT_FLOAT_EQ(transformed_point[0], 6.0);
+    EXPECT_FLOAT_EQ(transformed_point[1], 12.0);
+    EXPECT_FLOAT_EQ(transformed_point[2], 18.0);
+}
+
+TEST(NonlinearRefinement, TestTransformPointsRotation) {
+    Eigen::Vector<double, 6> const tf{0, 0, M_PI_2, 0, 0, 0};  // Rotation only
+    Eigen::Vector<double, 3> const point{5, 10, 15};
+
+    Eigen::Vector<double, 3> const transformed_point{TransformPoint(tf, point)};
+
+    EXPECT_FLOAT_EQ(transformed_point[0], -10.0);
+    EXPECT_FLOAT_EQ(transformed_point[1], 5.0);
+    EXPECT_FLOAT_EQ(transformed_point[2], 15.0);
+}
 
 TEST(NonlinearRefinement, TestPinholeProjection) {
     Eigen::Array<double, 4, 1> const pinhole_intrinsics{600, 600, 360, 240};
