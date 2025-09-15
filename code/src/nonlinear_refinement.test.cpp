1a4a6c21ceee7e31f32c97cc6528164ea00095f1
reprojection-calibration <reprojection.calibration@gmail.com>
2025-09-15 18:00:21 +0200

    add PinholeCostFunction

--- code/src/nonlinear_refinement.test.cpp
+++ code/src/nonlinear_refinement.test.cpp
@@ -27,8 +27,11 @@ Eigen::Vector<T, 2> PinholeProjection(T const* const camera, Eigen::Vector<T, 3>
 }
 
 // TODO(Jack): Can we use the se3 type here?
+// NOTE(Jack): We use Eigen::Ref here so we can pass both maps (in the PinholeCostFunction.operator()) and the direct
+// types (in the testing for example).
 template <typename T>
-Eigen::Vector<T, 3> TransformPoint(Eigen::Vector<T, 6> const& tf, Eigen::Vector<T, 3> const& point) {
+Eigen::Vector<T, 3> TransformPoint(Eigen::Ref<Eigen::Vector<T, 6> const> const& tf,
+                                   Eigen::Ref<Eigen::Vector<T, 3> const> const& point) {
     Eigen::Vector<T, 3> const axis_angle{tf.topRows(3)};
     Eigen::Vector<T, 3> transformed_point;
     ceres::AngleAxisRotatePoint(axis_angle.data(), point.data(), transformed_point.data());
@@ -39,6 +42,38 @@ Eigen::Vector<T, 3> TransformPoint(Eigen::Vector<T, 6> const& tf, Eigen::Vector<
     return transformed_point;
 }
 
+// Relation between eigen and ceres: https://groups.google.com/g/ceres-solver/c/7ZH21XX6HWU
+struct PinholeCostFunction {
+    explicit PinholeCostFunction(double const u, double const v) : u_{u}, v_{v} {}
+
+    // This is the contact line were ceres requirements for using raw pointers hits our desire to use more expressive
+    // eigen types. That is why here in the operator() we find the usage of the Eigen::Map class.
+    template <typename T>
+    bool operator()(T const* const pinhole_intrinsics, T const* const input_pose, T const* const input_point,
+                    T* const residual) const {
+        // WARN(Jack): Is there a way to canonically force the program to make sure the right memory is allocated and
+        // referenced to by these raw pointers? Ceres forces this pointer interface so I don't think we can easily do
+        // that but consider how we  can design the program to handle that automatically.
+        Eigen::Map<Eigen::Vector<T, 6> const> pose(input_pose);
+        Eigen::Map<Eigen::Vector<T, 3> const> point(input_point);
+        Eigen::Vector<T, 3> const point_co{TransformPoint<T>(pose, point)};
+
+        Eigen::Vector<T, 2> const pixel{PinholeProjection(pinhole_intrinsics, point_co)};
+
+        residual[0] = T(u_) - pixel[0];
+        residual[1] = T(v_) - pixel[1];
+
+        return true;
+    }
+
+    static ceres::CostFunction* Create(double const u, double const v) {
+        return new ceres::AutoDiffCostFunction<PinholeCostFunction, 2, 4, 6, 3>(new PinholeCostFunction(u, v));
+    }
+
+    double u_;
+    double v_;
+};
+
 }  // namespace reprojection_calibration::pnp
 
 using namespace reprojection_calibration::pnp;
@@ -47,7 +82,7 @@ TEST(NonlinearRefinement, TestTransformPointsTranslation) {
     Eigen::Vector<double, 6> const tf{0, 0, 0, 1, 2, 3};  // Translation only
     Eigen::Vector3d const point{5, 10, 15};
 
-    Eigen::Vector<double, 3> const transformed_point{TransformPoint(tf, point)};
+    Eigen::Vector<double, 3> const transformed_point{TransformPoint<double>(tf, point)};
 
     EXPECT_FLOAT_EQ(transformed_point[0], 6.0);
     EXPECT_FLOAT_EQ(transformed_point[1], 12.0);
@@ -58,7 +93,7 @@ TEST(NonlinearRefinement, TestTransformPointsRotation) {
     Eigen::Vector<double, 6> const tf{0, 0, M_PI_2, 0, 0, 0};  // Rotation only
     Eigen::Vector<double, 3> const point{5, 10, 15};
 
-    Eigen::Vector<double, 3> const transformed_point{TransformPoint(tf, point)};
+    Eigen::Vector<double, 3> const transformed_point{TransformPoint<double>(tf, point)};
 
     EXPECT_FLOAT_EQ(transformed_point[0], -10.0);
     EXPECT_FLOAT_EQ(transformed_point[1], 5.0);
