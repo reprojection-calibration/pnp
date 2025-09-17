4d44dd44d88f8bbc91bb99bd43d1558f9a451cd5
reprojection-calibration <reprojection.calibration@gmail.com>
2025-09-17 13:22:02 +0200

    add src folder as cppcheck header folder

--- building/scripts/code_checks.sh
+++ building/scripts/code_checks.sh
@@ -2,7 +2,9 @@
 
 set -eoux pipefail
 
+# Shellcheck
 find /temporary/building -iname '*.sh' -print0 | xargs --null shellcheck
-
+# Clang format
 find /temporary/code \( -iname '*.cpp' -o -iname '*.hpp' \) -print0 | xargs --null clang-format --dry-run --Werror
-cppcheck /temporary/code --enable=all --error-exitcode=1 -I /temporary/code/include --suppress=missingIncludeSystem
+# Cppcheck
+cppcheck /temporary/code --enable=all --error-exitcode=1 -I /temporary/code/include -I /temporary/code/src --suppress=missingIncludeSystem
